d7cf58ff1ec93632debd5271e0d28db085ec4fde
Jura Gorohovsky <gorohoroh@gmail.com>
2021-04-16 17:02:14 +0300

    Actual repro code

--- XamlDataContextCompletionRepro/MainWindow.xaml.cs
+++ XamlDataContextCompletionRepro/MainWindow.xaml.cs
@@ -1,13 +1,26 @@
-﻿namespace XamlDataContextCompletionRepro
+﻿using System.Collections.Generic;
+
+namespace XamlDataContextCompletionRepro
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
     {
-      InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyItemsControl.ItemsSource = new List<MyType>
+            {
+                new MyType {Text = "A"},
+                new MyType {Text = "B"},
+                new MyType {Text = "C"}
+            };
+        }
     }
-  }
+
+    public class MyType
+    {
+        public string Text { get; set; }
+    }
 }
